fb172a767b560795c5705ee454f88d848df0e1b2
riswu <sugike0301@gmail.com>
2021-02-16 17:15:37 +0900

    .emacs.d を削除

--- link.sh
+++ link.sh
@@ -4,5 +4,4 @@ ln -snf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -snf ~/dotfiles/.bashrc ~/.bashrc
 ln -snf ~/dotfiles/.config/fish ~/.config/fish
 ln -snf ~/dotfiles/.config/karabiner ~/.config/karabiner
-ln -snf ~/dotfiles/.emacs.d ~/.emacs.d
 ln -snf ~/dotfiles/Library/Application\ Support/Code/User ~/Library/Application\ Support/Code/User
